c80b2ee831c53ec34e006099ec50df2a10d29f0d
throskam <throskam@gmail.com>
2014-01-13 19:43:26 +0100

    Refactoring
    Refactor permissions policy to be more modular.

--- api/policies/permissions.js
+++ api/policies/permissions.js
@@ -1,118 +1,101 @@
-/**
- * REFACTOR:
- *
- * 	- transform check
- * 		- accept async
- * 		- diff find method
- * 	- find
- * 		- add find for other stuff
- * 	- is*
- * 		- just the test
- */
 
 module.exports = function (req, res, next) {
 
-	var find = function (where, cb) { Participant.findOne(where).done(cb); }
+	var findParticipant = function (where, cb) { Participant.findOne(where).done(cb); }
+	var findTopic = function (where, cb) { Topic.findOne(where).done(cb); }
+	var findMessage = function (where, cb) { Message.findOne(where).done(cb); }
 
-	var check = function (where, test, cb) {
+	var check = function (find, where, test, cb) {
 		find(where, function (err, participant) {
 			if (err) return cb(err);
-			if (participant && test(participant)) return cb(null, true);
+			if (participant) return test(participant, cb);
 			return cb(null, false);
 		});
 	}
 
-	var isMetaMember = function (where, cb) { check(where, function (participant) { return participant.isMetaMember(); }, cb); }
-	var isMember = function (where, cb) { check(where, function (participant) { return participant.isMember(); }, cb); }
-	var isStaff = function (where, cb) { check(where, function (participant) { return participant.isStaff(); }, cb); }
+	var isMetaMember = function (participant, cb) { return cb(null, participant.isMetaMember()); };
+	var isMember = function(participant, cb) { return cb(null, participant.isMember()); };
+	var isStaff = function(participant, cb) { return cb(null, participant.isStaff()); };
 
 	return Bouncer()
 		.when('chat/read', function (param, cb) {
-			isMetaMember({ user: param('me').id, chat: param('chat') }, cb);
+			check(findParticipant, { user: param('me').id, chat: param('chat') }, isMetaMember, cb);
 		})
 
 		.when('chat/participants', function (param, cb) {
-			isMember({ user: param('me').id, chat: param('chat') }, cb);
+			check(findParticipant, { user: param('me').id, chat: param('chat') }, isMember, cb);
 		})
 
 		.when('chat/topics', function (param, cb) {
-			isMember({ user: param('me').id, chat: param('chat') }, cb);
+			check(findParticipant, { user: param('me').id, chat: param('chat') }, isMember, cb);
 		})
 
 		.when('chat/messages', function (param, cb) {
-			isMember({ user: param('me').id, chat: param('chat') }, cb);
+			check(findParticipant, { user: param('me').id, chat: param('chat') }, isMember, cb);
 		})
 
 		.when('chat/invite', function (param, cb) {
-			isStaff({ user: param('me').id, chat: param('chat') }, cb);
+			check(findParticipant, { user: param('me').id, chat: param('chat') }, isStaff, cb);
 		})
 
 		.when('participant/read', function (param, cb) {
-			find(param('participant'), function (err, participant) {
-				if (err) return cb(err);
-				isMember({ user: param('me').id, chat: participant.chat }, cb);
-			});
+			check(findParticipant, param('participant'), function (participant, ok) {
+				check(findParticipant, { user: param('me').id, chat: participant.chat }, isMember, ok);
+			}, cb);
 		})
 
 		.when('participant/join', function (param, cb) {
-			check(param('participant'), function (participant) { return participant.user == param('me').id; }, cb);
+			check(findParticipant, param('participant'), function (participant, ok) { return ok(null, participant.user == param('me').id); }, cb);
 		})
 
 		.when('participant/leave', function (param, cb) {
-			check(param('participant'), function (participant) { return participant.user == param('me').id; }, cb);
+			check(findParticipant, param('participant'), function (participant, ok) { return ok(null, participant.user == param('me').id); }, cb);
 		})
 
 		.when('participant/connect', function (param, cb) {
-			check(param('participant'), function (participant) { return participant.user == param('me').id; }, cb);
+			check(findParticipant, param('participant'), function (participant, ok) { return ok(null, participant.user == param('me').id); }, cb);
 		})
 
 		.when('participant/disconnect', function (param, cb) {
-			check(param('participant'), function (participant) { return participant.user == param('me').id; }, cb);
+			check(findParticipant, param('participant'), function (participant, ok) { return ok(null, participant.user == param('me').id); }, cb);
 		})
 
 		.when('participant/promote', function (param, cb) {
-			find(param('participant'), function (err, participant) {
-				if (err) return cb(err);
-				check({user: param('me').id }, function (me) { return me.isHigherThan(participant); }, cb);
+			check(findParticipant, param('participant'), function (participant, cb) {
+				check(findParticipant, {user: param('me').id }, function (me, ok) { return ok(null, me.isHigherThan(participant)); }, cb);
 			});
 		})
 
 		.when('participant/revoke', function (param, cb) {
-			find(param('participant'), function (err, participant) {
-				if (err) return cb(err);
-				check({user: param('me').id }, function (me) { return me.isHigherThan(participant); }, cb);
+			check(findParticipant, param('participant'), function (participant, cb) {
+				check(findParticipant, {user: param('me').id }, function (me, ok) { return ok(null, me.isHigherThan(participant)); }, cb);
 			});
 		})
 
 		.when('topic/create', function (param, cb) {
-			check(param('participant'), function (participant) { return participant.isMember() && participant.user == param('me').id; }, cb)
+			check(findParticipant, param('participant'), function (participant, ok) { return ok(null, participant.isMember() && participant.user == param('me').id); }, cb);
 		})
 
 		.when('topic/destroy', function (param, cb) {
-			Topic.findOne(param('topic')).done(function (err, topic) {
-				if (err) return cb(err);
-				check(topic.participant, function (participant) { return participant.user == param('me').id; }, cb);
-			});
+			check(findTopic, param('topic'), function (topic, done) {
+				check(findParticipant, topic.participant, function (participant, ok) { return ok(null, participant.user == param('me').id); }, done);
+			}, cb);
 		})
 
 		.when('message/create', function (param, cb) {
-			check(param('participant'), function (participant) { return participant.isMember() && participant.user == param('me').id; }, cb)
+			check(findParticipant, param('participant'), function (participant, ok) { return ok(null, participant.isMember() && participant.user == param('me').id); }, cb);
 		})
 
 		.when('message/subjects', function (param, cb) {
-			Message.findOne(param('message')).done(function (err, message) {
-				if (err) return cb(err);
-
-				isMember(message.participant, cb);
-			});
+			check(findMessage, param('message'), function (message, ok) {
+				check(findParticipant, message.participant, isMember, ok);
+			}, cb);
 		})
 
 		.when('message/recipients', function (param, cb) {
-			Message.findOne(param('message')).done(function (err, message) {
-				if (err) return cb(err);
-
-				isMember(message.participant, cb);
-			});
+			check(findMessage, param('message'), function (message, ok) {
+				check(findParticipant, message.participant, isMember, ok);
+			}, cb);
 		})
 
 	.bounce(req, res, next);
